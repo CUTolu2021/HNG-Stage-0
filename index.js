a463da14309e8d5f7a1792c7e2163d877ea8423e
Omoniyi Tolulope <omoniyitolulope05@gmail.com>
2025-01-31 20:23:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+let PORT = 3000
+
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    const response = {
+        email: "omoniyitolulope05@gmail.com", // Replace with your registered email
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/CUTolu21/your-repo" // Replace with your GitHub repo URL
+    };
+    res.send(response);
+})
+
+app.listen(PORT, () => {
+    console.log(`HNG Stage one app listening on port ${PORT}`)
+})

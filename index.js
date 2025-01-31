b780d4d4594ae5ef254fa4fd98a3fa2eb8b060e9
Omoniyi Tolulope <omoniyitolulope05@gmail.com>
2025-01-31 20:30:39 +0100

    Commit

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ let PORT = 3000
 
 app.use(cors())
 
-
 app.get('/', (req, res) => {
     const response = {
         email: "omoniyitolulope05@gmail.com", // Replace with your registered email
         current_datetime: new Date().toISOString(),
         github_url: "https://github.com/CUTolu21/your-repo" // Replace with your GitHub repo URL
     };
-    res.send(response);
+    res.status(200).send(response);
 })
 
 app.listen(PORT, () => {

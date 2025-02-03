089b66b3a06e29a93e79c3802da62b236ae30d87
Omoniyi Tolulope <omoniyitolulope05@gmail.com>
2025-02-03 15:24:44 +0100

    stage one

--- index.js
+++ index.js
@@ -6,76 +6,12 @@ let PORT = 3000
 
 app.use(cors())
 
-app.get('/api/classify-number', async (req, res) => {
-    const entry = parseInt(req.query.number, 10);
-
-    if (isNaN(entry)) {
-        return res.status(400).send(
-            {
-                number: 'alphabet',
-                error: true
-            });
-    }
-    let isPrime = entry > 1;
-    for (let i = 2; i <= Math.sqrt(entry); i++) {
-        if (entry % i === 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    function isPerfectNumber(number) {
-        if (number <= 1) return false;
-    
-        let sum = 0;
-        for (let i = 1; i <= number / 2; i++) {
-            if (number % i === 0) {
-                sum += i;
-            }
-        }
-    
-        return sum === number;
-    }
-
-    const isPerfect = isPerfectNumber(entry);
-
-    function isArmstrongNumber(number) {
-        const digits = number.toString().split('');
-        const numberOfDigits = digits.length;
-        const armstrongSum = digits.reduce((accumulator, digit) => {
-            return accumulator + Math.pow(parseInt(digit), numberOfDigits);
-        }, 0);
-        return armstrongSum === number;
-    }
-
-    console.log(isArmstrongNumber(entry))
-
-    function digitSum(number){
-        const digits = number.toString().split('');
-        const sum = digits.reduce((acc, digit) => acc + parseInt(digit), 0);
-        return sum;
-    }
-    const fun_fact_response = await fetch(`http://numbersapi.com/${entry}/math?json`)
-    const fun_fact = (await fun_fact_response.json()).text || 'No fun fact available.';
-
-    const properties = []
-    if (isArmstrongNumber(entry)){
-        properties.push("armstrong")
-    }
-    if (entry % 2 != 0){
-        properties.push("odd")
-    }else{
-        properties.push("even")
-    }
-
+app.get('/', (req, res) => {
     const response = {
-        number: entry,
-        is_prime: isPrime,
-        is_perfect: isPerfect,
-        properties,
-        digit_sum:digitSum(entry),
-        fun_fact
-        };
+        email: "omoniyitolulope05@gmail.com", // Replace with your registered email
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/CUTolu2021/HNG-Stage-0" // Replace with your GitHub repo URL
+    };
     res.status(200).send(response);
 })
 
